f4e55f5567f7a76c99ed44f212f18d9532fa4d68
valerofin <valerofin.111@gmail.com>
2026-01-18 20:21:41 +0300

    Change Music function

--- src/components/Outlet/Settings.jsx
+++ src/components/Outlet/Settings.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { motion as m } from 'motion/react';
 
 function Settings() {
-    const {volume, setVolume} = useMusic()
+    const {volume, setVolume, musicChange} = useMusic()
     const handleVolumeChange = (event) => {
       const newVolume = parseFloat(event.target.value);
       setVolume(newVolume);
@@ -21,7 +21,17 @@ function Settings() {
               max='1' 
               step='0.05' 
               value={volume} 
-              onChange={handleVolumeChange} />
+              onChange={handleVolumeChange}
+              className={sass.input} />
+
+            <m.button 
+              className={sass.ChangeMusicBtn} 
+              onClick={() => musicChange()} 
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Сменить трек
+            </m.button>
 
             <m.div 
               whileHover={{ scale: 1.2 }} 
